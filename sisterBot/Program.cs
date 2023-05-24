e94346e0d49b1da6976e97141daa7221b002e840
Begaiym Beksultanova <begaiym.beksultanova@it-academy.kg>
2023-05-24 20:44:50 +0600

    Добавьте файлы проекта.

---
+++ sisterBot/Program.cs
@@ -0,0 +1,58 @@
+﻿using sisterBot.keyboards;
+using System;
+using System.Collections.Generic;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+using Message = Telegram.Bot.Types.Message;
+using Update = Telegram.Bot.Types.Update;
+
+
+namespace SisterBot
+{
+    class Program
+    {
+        static ITelegramBotClient bot = new TelegramBotClient("6239769324:AAE8BbkP2v0d4dyNbx45UEkvBOuBmNdSs7g");
+
+        public static async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            // Некоторые действия
+            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(exception));
+        }
+        
+        public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            InlineKeyboardMarkup agreementButton2 = Agreement.agreementButton;
+
+            var message = update.Message;
+            if (message.Text.ToLower() == "/start")
+            {
+                await botClient.SendTextMessageAsync(update.CallbackQuery.Message.Chat.Id, "Disclaimer", replyMarkup: agreementButton2);
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Запущен бот " + bot.GetMeAsync().Result.FirstName);
+
+            var cts = new CancellationTokenSource();
+            var cancellationToken = cts.Token;
+            var receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = { }, // receive all update types
+            };
+            bot.StartReceiving(
+                HandleUpdateAsync,
+                HandleErrorAsync,
+                receiverOptions,
+                cancellationToken
+            );
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ sisterBot/keyboards/Agreement.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace sisterBot.keyboards
+{
+    public static class Agreement
+    {
+        
+
+       
+
+
+
+        public static InlineKeyboardMarkup agreementButton = new InlineKeyboardMarkup(
+                        new[]
+                        {
+                            // first row
+                            new[]
+                            {
+                                // first button in row
+                                InlineKeyboardButton.WithCallbackData(text: "Кнопка 3", callbackData: "post3"),
+                                // second button in row
+                                InlineKeyboardButton.WithCallbackData(text: "Кнопка 4", callbackData: "post4"),
+                            },
+
+                        });
+    }
+}
